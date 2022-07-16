4abd3617cbde11846027ff27a9e63150b165907f
anwaarulhaq07 <109423432+anwaarulhaq07@users.noreply.github.com>
2022-07-16 20:28:55 +0500

    blade files and controllers for rental properties

---
+++ app/Http/Controllers/Admin/rental/Outstanding_balanceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\admin\rental;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Outstanding_balanceController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Admin/rental/RentrollController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\admin\rental;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RentrollController extends Controller
+{
+    //
+}
